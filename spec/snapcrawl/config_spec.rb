1eddd149347dff7f62d1d3e929fd0c0a2b2bc465
Danny Ben Shitrit <db@dannyben.com>
2023-07-26 17:45:10 +0000

    allow some leeway in yaml comparison tests

--- spec/snapcrawl/config_spec.rb
+++ spec/snapcrawl/config_spec.rb
@@ -4,14 +4,14 @@ describe Config do
   subject { described_class }
 
   describe '#load' do
-    it 'has defaults' do
+    it 'has defaults', :focus do
       subject.load
-      expect(subject.settings.to_yaml).to match_approval('config/defaults')
+      expect(subject.settings.to_yaml).to match_approval('config/defaults').diff(4)
     end
 
     it 'loads file if it exists and merges it with the defaults' do
       subject.load 'spec/fixtures/config/minimal'
-      expect(subject.settings.to_yaml).to match_approval('config/minimal')
+      expect(subject.settings.to_yaml).to match_approval('config/minimal').diff(4)
     end
 
     context 'when the config file is not found' do
